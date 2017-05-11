1c68f646c16e67e764babbb9a80df65d68233c87
Omar Hydrah <omarhydrah@tutanota.de>
2017-05-11 20:09:51 +0200

    Final commit for Heroku Production

--- config/mysql-config.js
+++ config/mysql-config.js
@@ -1,10 +1,20 @@
 var mysql = require("mysql");
-var connection = mysql.createConnection({
+var localHost = {
 	host    : "localhost",
 	user    : "sakila_client",
 	password: "1234",
 	database: "sakila"
-});
+};
+
+var remoteHost = {
+	host    : "db4free.net",
+	user    : "omar_alsalahy",
+	password: "123456",
+	database: "omar_sakila"
+};
+var connection = mysql.createConnection(remoteHost);
+
+
 
 connection.connect();
 
